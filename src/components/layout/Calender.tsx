3157e5b4737ae7093c019b4fce702f18a45cdadb
Link1225 <kh7672ak@gmail.com>
2025-08-24 04:52:30 +0900

    a

--- src/components/layout/Calender.tsx
+++ src/components/layout/Calender.tsx
@@ -1,12 +1,15 @@
 import Days from "../ui/calender/Days";
 import Dates from "../ui/calender/Dates";
+import { Suspense } from "react";
 
 export default function Calender({index}: { index: number }) {
     return (
         <section className="flex-1 flex flex-col min-h-[50%]">
             <h2 className="flex justify-center item-center font-bold">{new Date().getMonth() + index + 1}月</h2>
             <Days />
-            <Dates index={index} />
+            <Suspense>
+                <Dates index={index} />
+            </Suspense>
         </section>
     )
 }
