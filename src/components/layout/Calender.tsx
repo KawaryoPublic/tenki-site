a879356f90f199f958de2910b5c13ee46c215e36
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-08-23 14:07:43 +0900

    a

--- src/components/layout/Calender.tsx
+++ src/components/layout/Calender.tsx
@@ -4,14 +4,23 @@ export default function Calender() {
     return (
         <section>
             {
+                ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"].map((day, index) => {
+                    return (
+                        <div key={index} className="w-20 h-20 m-1 p-2 border rounded font-bold text-center">
+                            {day}
+                        </div>
+                    );
+                })
+            }
+            {
                 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((date, index) => {
                     return (
                         <button
                             key={index}
-                            className="w-20 h-20 m-1 p-2 border rounded"
+                            className="w-20 h-20 m-1 p-2 border rounded text-center"
                             onClick={e => {alert(`You clicked on date ${date}`)}}
                         >{date}</button>
-                    )
+                    );
                 })
             }
         </section>
