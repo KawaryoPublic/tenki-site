b864a27a59429e179db60754b91b89b75c9104e2
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-08-23 13:05:03 +0900

    a

--- src/components/layout/Files.tsx
+++ src/components/layout/Files.tsx
@@ -30,7 +30,7 @@ export default function Files() {
                                 </Link>
                             </li>
                         );
-                    });
+                    })
                 }
             </ul>
         </section>
