d111b262e29415321bc0abadaf6e0333bc9815bc
Link1225 <kh7672ak@gmail.com>
2025-08-23 10:46:17 +0900

    a

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import { HeaderNav } from "../ui/HeaderNav";
 
 export default function Header() {
   return (
     <header className="top-0 left-0 w-full p-4">
       <h1 className="text-3xl">天文気象部</h1>
-      <HeaderNav />
+      <Suspense>
+        <HeaderNav />
+      </Suspense>
     </header>
   );
 }
