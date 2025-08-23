151205906b1107b78c91fb0fd3522acc8b9d3892
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-08-23 14:37:52 +0900

    a

--- src/components/ui/calender/Dates.tsx
+++ src/components/ui/calender/Dates.tsx
@@ -1,37 +1,18 @@
 "use client";
 
 export default function Dates() {
+    const firstDay = new Date(new Date().getFullYear(), new Date().getMonth(), 1).getDay();
+
+    const dates = Array.from({ length: 42 }, (_, i) => i + 1 - firstDay);
+
     return (
         <div>
             {
-                <div>
-                    {
-                        Array.from({ length: 6 }).map((_, index) => {
-                            return (
-                                <div>
-                                    {
-                                        Array.from({ length: 7 }).map((_, index) => {
-                                            return (
-                                                <button
-                                                    key={index}
-                                                    className="w-20 h-20 m-1 p-2 border rounded text-center"
-                                                    onClick={e => {alert(`You clicked on date ${date}`)}}
-                                                >{date}</button>
-                                            );
-                                        })
-                                    }
-                                </div>
-                            );
-                        })
-                    }
-                </div>
-                [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((date, index) => {
+                dates.map((date, index) => {
                     return (
-                        <button
-                            key={index}
-                            className="w-20 h-20 m-1 p-2 border rounded text-center"
-                            onClick={e => {alert(`You clicked on date ${date}`)}}
-                        >{date}</button>
+                        <button key={index} className="w-20 h-20 m-1 p-2 border rounded text-center">
+                            {date > 0 ? date : ""}
+                        </button>
                     );
                 })
             }
