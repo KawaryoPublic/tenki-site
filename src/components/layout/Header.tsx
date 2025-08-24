9b8853b6804081687761ca39e42bcb29ac43ee3e
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-08-24 14:38:57 +0900

    a

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,10 +1,17 @@
 import { Suspense } from "react";
 import { HeaderNav } from "../ui/HeaderNav";
 import { Passwords } from "@/lib/type";
-import { fetchJson } from "@/lib/action";
+import { headers } from "next/headers";
 
 export default async function Header() {
-  const passwords: Passwords = await fetchJson("/api/passwords.json");
+  //const passwords: Passwords = await fetchJson("/api/passwords.json");
+  const headersData = await headers();
+  const res = await fetch(`${headersData.get("x-forwarded-proto") ?? "http"}://${headersData.get("host")}/api/passwords.json`);
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const passwords: Passwords = await res.json();
 
   return (
     <header className="top-4 left-0 w-full pt-4 pl-4">
