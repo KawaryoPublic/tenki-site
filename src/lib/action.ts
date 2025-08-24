d5cf75543c1ea04266655185648dd92aed7b5a6d
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-08-24 14:29:58 +0900

    a

--- src/lib/action.ts
+++ src/lib/action.ts
@@ -2,7 +2,7 @@ import { headers } from "next/headers";
 
 export async function fetchJson(url: string) {
     const headersData = headers();
-    const res = await fetch(`${headersData.get("x-forwarded-proto") ?? "http"}://${headersData.get("host")}${url}`);
+    const res = await fetch(`${headersData["x-forwarded-proto"] ?? "http"}://${headersData["host"]}${url}`);
     if (!res.ok) {
         throw new Error("Failed to fetch data");
     }
