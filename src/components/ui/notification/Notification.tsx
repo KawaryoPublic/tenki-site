919d76562316cb80a98bcf7dd7df3341aaa3f00c
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-09-01 16:49:11 +0900

    a

--- src/components/ui/notification/Notification.tsx
+++ src/components/ui/notification/Notification.tsx
@@ -2,7 +2,7 @@ export default function notification({ title, createdAt, content }: { title: str
     return (
         <div className="border p-4 rounded shadow-md w-64 bg-white">
             <h3 className="font-bold mb-2">{title}</h3>
-            <p>{createdAt}</p>
+            <p>{createdAt.toString()}</p>
             <p>{content}</p>
         </div>
     );
