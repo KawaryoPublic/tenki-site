84fa02ac2c3c08549666e346a96cedbe8dccde58
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-09-01 16:35:54 +0900

    a

--- src/components/ui/calender/date_info/DeletePlanButton.tsx
+++ src/components/ui/calender/date_info/DeletePlanButton.tsx
@@ -1,8 +1,5 @@
 "use client";
 
-import { redirect } from "next/navigation";
-import { useSearchParams } from "next/navigation";
-
 export default function DeletePlanButton({ id }: { id: number }) {
     return (
         <button
@@ -14,8 +11,6 @@ export default function DeletePlanButton({ id }: { id: number }) {
                     body: JSON.stringify({ id }),
                 }).then(() => alert("削除しました"))
                 .catch(err => console.log(err));
-
-                await redirect(`/calender?q=${useSearchParams().get("q")}`);
             }}
             className="bg-red-500 text-white px-4 py-2 rounded"
         >
