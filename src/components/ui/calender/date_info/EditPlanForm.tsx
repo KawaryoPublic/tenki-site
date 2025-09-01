bfb7120dc02d4279177ca831a0a21f20e4bb3e30
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-09-01 15:39:18 +0900

    a

--- src/components/ui/calender/date_info/EditPlanForm.tsx
+++ src/components/ui/calender/date_info/EditPlanForm.tsx
@@ -33,4 +33,5 @@ export default function EditPlanForm({ info }: { info: DateInfo }) {
                 <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">保存</button>
             </div>
         </Form>
-    )
+    );
+}
