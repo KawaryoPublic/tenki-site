03687752d4e7bc4494bfdd10ec3afc9d03ca3447
Link1225 <kh7672ak@gmail.com>
2025-09-01 13:09:07 +0900

    a

--- src/components/layout/DateInfoSection.tsx
+++ src/components/layout/DateInfoSection.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { DateInfo } from "@/lib/type";
-import { useEffect, useState } from "react"
+import { Suspense, useEffect, useState } from "react"
 import Form from "next/form";
 import RestrictedContent from "../ui/global/RestrictedContent";
 import DefaultLink from "../ui/global/DefaultLink";
@@ -28,47 +28,49 @@ export default function DateInfoSection({ id }: { id: number }) {
                     予定: {info?.plan ? info.plan : "なし"}
                 </div>
             </div>
-            <RestrictedContent>
-                <div className="pt-4">
-                    <h1 className="text-lg">編集</h1>
-                    <Form 
-                        action={async (formData) => {
-                            await fetch("/api/dateInfo", {
-                                method: "PUT",
-                                body: JSON.stringify({
-                                    id: info.id,
-                                    date: info.date,
-                                    club: formData.get("club") === "on",
-                                    plan: formData.get("plan")
-                                }),
-                            }).catch(err => console.log(err));
-
-                            window.location.reload();
-                        }}
-                    >
-                        <div>
-                            <label htmlFor="club">部活: </label>
-                            <input 
-                                type="checkbox" 
-                                name="club" 
-                            />
-                        </div>
-                        <div>
-                            <label htmlFor="plan">予定: </label>
-                            <textarea 
-                                name="plan" 
-                                rows={10}
-                                cols={100}
-                                className="bg-white"
-                            >
-                            </textarea>
-                        </div>
-                        <div>
-                            <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">保存</button>
-                        </div>
-                    </Form>
-                </div>
-            </RestrictedContent>
+            <Suspense>
+                <RestrictedContent>
+                    <div className="pt-4">
+                        <h1 className="text-lg">編集</h1>
+                        <Form 
+                            action={async (formData) => {
+                                await fetch("/api/dateInfo", {
+                                    method: "PUT",
+                                    body: JSON.stringify({
+                                        id: info.id,
+                                        date: info.date,
+                                        club: formData.get("club") === "on",
+                                        plan: formData.get("plan")
+                                    }),
+                                }).catch(err => console.log(err));
+                            
+                                window.location.reload();
+                            }}
+                        >
+                            <div>
+                                <label htmlFor="club">部活: </label>
+                                <input 
+                                    type="checkbox" 
+                                    name="club" 
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="plan">予定: </label>
+                                <textarea 
+                                    name="plan" 
+                                    rows={10}
+                                    cols={100}
+                                    className="bg-white"
+                                >
+                                </textarea>
+                            </div>
+                            <div>
+                                <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded">保存</button>
+                            </div>
+                        </Form>
+                    </div>
+                </RestrictedContent>
+            </Suspense>
             <div>
                 <DefaultLink href="/calender">日付一覧に戻る</DefaultLink>
             </div>
