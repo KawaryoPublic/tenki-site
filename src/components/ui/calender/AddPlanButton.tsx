749250b172a7dde3d933420f7c40e3c3001f1598
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-09-01 16:24:59 +0900

    a

--- src/components/ui/calender/AddPlanButton.tsx
+++ src/components/ui/calender/AddPlanButton.tsx
@@ -1,11 +1,18 @@
 "use client";
 
+import { DateInfo } from "@/lib/type";
+import { useState } from "react";
+import { redirect } from "next/navigation";
+
 export default function AddPlanButton({ date }: { date: Date }) {
+    const [ info, setInfo ] = useState<DateInfo>({id: -1, date: ""});
+
     return (
         <button
             className="w-full h-full"
             onClick={async () => {
-                confirm("予定を追加しますか？") &&
+                if(!confirm("予定を追加しますか？")) return;
+
                 await fetch("/api/dateInfo", {
                     method: "POST",
                     body: JSON.stringify({
@@ -13,8 +20,14 @@ export default function AddPlanButton({ date }: { date: Date }) {
                         plan: ""
                     }),
                 }).then(() => alert("追加しました"))
-                .then(() => window.location.reload())
-                .catch(err => console.log(err))
+                .catch(err => console.log(err));
+
+                await fetch('/api/dateInfo')
+                    .then(res => res.json())
+                    .then(data => setInfo(data.find(info => info.date === `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`)))
+                    .catch(err => console.log(err));
+
+                await redirect(`/calender/${info.id}`);
             }}
         />
     )
