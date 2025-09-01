a0e3703d9312cac4f756695bd80ec649dca7c6b5
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-09-01 17:27:17 +0900

    a

--- src/app/(tab)/notification/page.tsx
+++ src/app/(tab)/notification/page.tsx
@@ -1,7 +1,7 @@
 import NotificationSection from "@/components/layout/notification/NotificationSection";
 import DefaultLink from "@/components/ui/global/DefaultLink";
 import RestrictedContent from "@/components/ui/global/RestrictedContent";
-import AddNotificationButton from "@/components/ui/notification/AddNotificationButton";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
@@ -9,9 +9,11 @@ export default function Home() {
       <div className="flex justify-between items-center mb-4">
         <h1 className="text-3xl">通知ページ</h1>
         <div>
-          <RestrictedContent>
-            <DefaultLink href="/notification/edit">追加</DefaultLink>
-          </RestrictedContent>
+          <Suspense>
+            <RestrictedContent>
+              <DefaultLink href="/notification/edit">追加</DefaultLink>
+            </RestrictedContent>
+          </Suspense>
         </div>
       </div>
       <NotificationSection />
