56d8093c27d36d7c088a1031d69a17774899a448
Link1225 <kh7672ak@gmail.com>
2025-09-01 13:11:29 +0900

    a

--- src/app/(tab)/calender/page.tsx
+++ src/app/(tab)/calender/page.tsx
@@ -1,18 +1,8 @@
 import CalenderSection from "@/components/layout/CalenderSection";
-import DefaultLink from "@/components/ui/global/DefaultLink";
-import RestrictedContent from "@/components/ui/global/RestrictedContent";
-import { Suspense } from "react";
 
 export default function Home() {
     return (
         <div className="flex flex-col space-x-4 lg:flex-row lg:space-y-4 w-full">
-            <div className="p-2">
-                <Suspense>
-                    <RestrictedContent>
-                        <DefaultLink href="/calender/edit" className="border p-2">予定の編集</DefaultLink>
-                    </RestrictedContent>
-                </Suspense>
-            </div>
             {
                 Array.from({ length: 3 }, (_, i) => i).map(i => (
                     <CalenderSection key={i} index={i} />
