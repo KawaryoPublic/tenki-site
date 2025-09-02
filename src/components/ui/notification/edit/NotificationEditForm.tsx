756507872ffec731489837c1e8e08495dd57b72b
KawaryoPublic <kawaryo.tenki.public@gmail.com>
2025-09-02 13:37:28 +0900

    Update NotificationEditForm.tsx

--- src/components/ui/notification/edit/NotificationEditForm.tsx
+++ src/components/ui/notification/edit/NotificationEditForm.tsx
@@ -16,21 +16,22 @@ export default function NotificationEditForm() {
                 }).then(() => alert("告知を追加しました"))
                 .catch(err => console.log(err));
             }}
+            className="w-full"
         >   
-            <div>
-                <label htmlFor="title">タイトル:</label><br />
+            <div className="w-full pb-2">
+                <label htmlFor="title" className="font-bold">タイトル:</label><br />
                 <textarea 
                     name="title" 
                     rows={1} 
-                    className="bg-white w-full"
+                    className="bg-white resize-none w-full"
                 />
             </div>
-            <div>
-                <label htmlFor="content">内容:</label><br />
+            <div className="w-full pb-2">
+                <label htmlFor="content" className="font-bold">内容:</label><br />
                 <textarea 
                     name="content" 
                     rows={10}
-                    className="bg-white whitespace-pre-wrap w-full"
+                    className="bg-white resize-none w-full"
                 />
             </div>
             <div>
