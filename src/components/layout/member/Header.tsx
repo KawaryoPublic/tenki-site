dfb344a912f79f387884e204bf9f6aed3c76a01c
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-09-09 14:28:27 +0900

    i

--- src/components/layout/member/Header.tsx
+++ src/components/layout/member/Header.tsx
@@ -1,6 +1,7 @@
 import RestrictedLink from "../../ui/global/RestrictedLink";
 import RestrictedContent from "../../ui/global/RestrictedContent";
 import Link from "next/link";
+import { Suspense } from "react";
 
 export default async function Header() {
   return (
@@ -8,35 +9,36 @@ export default async function Header() {
         <h1 className="text-xl lg:text-3xl">天文気象部</h1>
         <nav>
             <ul className="flex text-base lg:text-xl space-x-3 lg:space-x-4 p-2 lg:p-4">
-                <li>
-                    <Link href="/">ホーム</Link>
-                </li>
-                <RestrictedContent allowParent allowStudent>
+                <Suspense>
                     <li>
-                        <RestrictedLink href="/notification">告知</RestrictedLink>
+                        <Link href="/">ホーム</Link>
                     </li>
-                </RestrictedContent>
-                <RestrictedContent allowStudent>
-                    <li>
-                        <RestrictedLink href="/calendar">カレンダー</RestrictedLink>
-                    </li>
-                </RestrictedContent>
-                <RestrictedContent allowStudent>
-                    <li>
-                        <RestrictedLink href="/equipment">機材</RestrictedLink>
-                    </li>
-                </RestrictedContent>
-                <RestrictedContent allowStudent>
-                    <li>
-                        <RestrictedLink href="/manual">マニュアル</RestrictedLink>
-                    </li>
-                </RestrictedContent>
-                <RestrictedContent>
-                    <li>
-                        <RestrictedLink href="/file">ファイル</RestrictedLink>
-                    </li>
-                </RestrictedContent>
-
+                    <RestrictedContent allowParent allowStudent>
+                        <li>
+                            <RestrictedLink href="/notification">告知</RestrictedLink>
+                        </li>
+                    </RestrictedContent>
+                    <RestrictedContent allowStudent>
+                        <li>
+                            <RestrictedLink href="/calendar">カレンダー</RestrictedLink>
+                        </li>
+                    </RestrictedContent>
+                    <RestrictedContent allowStudent>
+                        <li>
+                            <RestrictedLink href="/equipment">機材</RestrictedLink>
+                        </li>
+                    </RestrictedContent>
+                    <RestrictedContent allowStudent>
+                        <li>
+                            <RestrictedLink href="/manual">マニュアル</RestrictedLink>
+                        </li>
+                    </RestrictedContent>
+                    <RestrictedContent>
+                        <li>
+                            <RestrictedLink href="/file">ファイル</RestrictedLink>
+                        </li>
+                    </RestrictedContent>
+                </Suspense>
             </ul>
         </nav>
     </header>
