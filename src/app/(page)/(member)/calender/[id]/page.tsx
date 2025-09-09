ba36de09ffab11db13f9bcf976cf62aae405dc39
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-09-09 14:12:31 +0900

    g

--- src/app/(page)/(member)/calender/[id]/page.tsx
+++ src/app/(page)/(member)/calender/[id]/page.tsx
@@ -1,7 +1,9 @@
 import DateInfoSection from "@/components/layout/member/calender/DateInfoSection";
 
-export default async function Home({ params,  }: { params: { id: number }, searchParams: { q: string } }) {
-    <div className="w-full flex flex-col">
-        <DateInfoSection id={(await params).id} q={"(await searchParams).q"} /> 
-    </div>
+export default async function Home({ params, searchParams }: { params: { id: number }, searchParams: { q: string } }) {
+    return (
+        <div className="w-full flex flex-col">
+            <DateInfoSection id={(await params).id} q={(await searchParams).q} /> 
+        </div>
+    );
 }
