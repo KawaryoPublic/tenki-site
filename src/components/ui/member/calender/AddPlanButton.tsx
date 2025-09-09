b6d05c3fc648220fe190ea0a9f3b3bc251226d7e
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-09-09 13:58:13 +0900

    a

--- src/components/ui/member/calender/AddPlanButton.tsx
+++ src/components/ui/member/calender/AddPlanButton.tsx
@@ -1,14 +1,9 @@
 "use client";
 
 import { EXECUTIVE_PASSWORD } from "@/lib/const";
-import { DateInfo } from "@/lib/type";
 import { useSearchParams } from "next/navigation";
-import { useState } from "react";
-import { redirect } from "next/navigation";
 
 export default function AddPlanButton({ date, firstDate }: { date: Date, firstDate: Date }) {
-    const [ dateInfo, setDateInfo ] = useState<DateInfo>({id: -1, date: ""});
-
     return (
         <button
             className={`
@@ -25,11 +20,8 @@ export default function AddPlanButton({ date, firstDate }: { date: Date, firstDa
                         date: `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`,
                         plan: ""
                     }),
-                }).then(res => res.json())
-                .then(data => setDateInfo(data))
+                }).then(() => window.location.reload())
                 .catch(err => console.log(err));
-
-                redirect(`/calender/${dateInfo.id}`);
             }}
         >
             {date.getDate()}
