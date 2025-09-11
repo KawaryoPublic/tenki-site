f3e9552a86abe0944a6ca8d67d6037d8053b50b4
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-09-11 17:23:49 +0900

    a

--- src/components/ui/member/calendar/Days.tsx
+++ src/components/ui/member/calendar/Days.tsx
@@ -1,15 +1,16 @@
 import Link from "next/link";
 import RestrictedLink from "../../global/RestrictedLink";
+import { DAYS } from "@/lib/const";
 
 export default function Days() {
     return (
-        <div className="pt-3 pb-3 grid grid-cols-7 gap-2">
+        <div className="pt-3 pb-3 flex gap-2">
             {
-                ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"].map((day, index) => {
+                DAYS.map((day, index) => {
                     return (
                         <RestrictedLink 
                             key={index} 
-                            className="flex items-center justify-center rounded"
+                            className="w-full h-full font-bold flex items-center justify-center bg-white rounded"
                             href={`/calendar/edit_observation/${index}`}
                         >
                             {day}
--- src/lib/const.ts
+++ src/lib/const.ts
@@ -2,4 +2,6 @@ const STUDENT_PASSWORD = "student";
 const PARENT_PASSWORD = "parent";
 const EXECUTIVE_PASSWORD = "executive";
 
-export { STUDENT_PASSWORD, PARENT_PASSWORD, EXECUTIVE_PASSWORD };
+const DAYS = ['日', '月', '火', '水', '木', '金', '土'];
+
+export { STUDENT_PASSWORD, PARENT_PASSWORD, EXECUTIVE_PASSWORD, DAYS };
