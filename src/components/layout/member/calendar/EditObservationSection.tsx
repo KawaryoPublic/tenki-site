b1738e955dbfb9011ac0587f42f344a0afa988ea
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-09-11 17:27:20 +0900

    a

--- src/components/layout/member/calendar/EditObservationSection.tsx
+++ src/components/layout/member/calendar/EditObservationSection.tsx
@@ -1,9 +1,10 @@
 import EditObservationForm from "@/components/ui/member/calendar/edit_observation/EditObservationForm";
+import { DAYS } from "@/lib/const";
 
 export default function EditObservationSection({ day }: { day: number }) {
     return (
         <section>
-            <h1 className="text-2xl">{day}日の観測編集</h1>
+            <h1 className="text-2xl">{DAYS[Number(day)]}曜日の観測</h1>
             <div>
                 <EditObservationForm day={day}/>
             </div>
