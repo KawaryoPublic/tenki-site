810d22df7669f38245af24261260c0bbf4083cd8
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-09-11 15:37:58 +0900

    a

--- src/components/layout/member/notification/EditNotificationSection.tsx
+++ src/components/layout/member/notification/EditNotificationSection.tsx
@@ -7,7 +7,7 @@ export default function EditNotificationSection({ q }: { q: string }) {
     return (
         <section className="flex flex-col gap-4">
             <WhiteFrame className="flex flex-col gap-2">
-                <h2 className="text-xl border-b"></h2>
+                <h2 className="text-xl border-b">告知を追加</h2>
                 <NotificationEditForm q={q} />
             </WhiteFrame>
             <div>
