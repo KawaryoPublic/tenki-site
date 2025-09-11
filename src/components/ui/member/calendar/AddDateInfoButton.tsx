272a9e22c1d517ad3d5b2353216e195eeaecd071
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-09-11 17:10:43 +0900

    a

--- src/components/ui/member/calendar/AddDateInfoButton.tsx
+++ src/components/ui/member/calendar/AddDateInfoButton.tsx
@@ -6,7 +6,7 @@ import { useSearchParams } from "next/navigation";
 export default function AddDateInfoButton({ date }: { date: Date }) {
     return (
         <button
-            className="w-full h-full text-center"
+            className="w-full h-full flex items-center justify-center"
             onClick={async () => {
                 if(useSearchParams().get("q") !== EXECUTIVE_PASSWORD) alert(useSearchParams().get("q") + "は違います");
                 if(!confirm("予定を追加しますか？")) return;
