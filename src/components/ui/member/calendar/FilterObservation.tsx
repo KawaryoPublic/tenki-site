43c4cd5822b24eaaa909c868774031988ba8689f
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-09-12 15:35:38 +0900

    q

--- src/components/ui/member/calendar/FilterObservation.tsx
+++ src/components/ui/member/calendar/FilterObservation.tsx
@@ -4,11 +4,14 @@ import DefaultTextArea from "../../global/DefaultTextArea";
 import Form from "next/form"
 import { redirect } from "next/navigation";
 import BlueButton from "../../global/button/BlueButton";
+import { useSearchParams } from "next/navigation";
 
 export default function FilterObservation() {
+    const q = useSearchParams().get("q");
+
     return (
         <Form
-            action={formData => redirect(`/calendar?filter=${formData.get('filter')}`)}
+            action={formData => redirect(`/calendar?q=${q}&filter=${formData.get('filter')}`)}
             className="flex flex-row"
         >
             <DefaultTextArea
