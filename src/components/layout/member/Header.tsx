6c9b91bd4cf9922d03f4bcf65e5587cb502541ee
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-09-12 14:25:25 +0900

    a

--- src/components/layout/member/Header.tsx
+++ src/components/layout/member/Header.tsx
@@ -5,7 +5,7 @@ import { Suspense } from "react";
 
 export default async function Header() {
   return (
-    <header className="top-4 left-0 w-full">
+    <header className="top-4 left-0 w-full border-b">
         <h1 className="text-xl lg:text-3xl">天文気象部</h1>
         <nav>
             <ul className="flex text-base lg:text-xl space-x-3 lg:space-x-4 p-2 lg:p-4">
