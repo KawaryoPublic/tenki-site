301dd7f2da7194581f603a31813936804db51f1b
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-09-13 12:47:03 +0900

    a

--- src/components/ui/global/RestrictedLink.tsx
+++ src/components/ui/global/RestrictedLink.tsx
@@ -5,6 +5,6 @@ import { useSearchParams } from "next/navigation";
 
 export default function RestrictedLink({ href, children, className, otherParams }: { href: string, children: React.ReactNode, className?: string, otherParams?: string}) {
     return (
-        <Link href={`${href}?q=${useSearchParams().get("q")}&${otherParams}`} className={className}>{children}</Link>
+        <Link href={`${href}?q=${useSearchParams().get("q")}${otherParams ? "&" + otherParams : ""}`} className={className}>{children}</Link>
     )
 }
