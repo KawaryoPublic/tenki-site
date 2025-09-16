cfaaa67c9398944dd521156411a7ffefd9dd2de7
Link1225 <kh7672ak@gmail.com>
2025-09-17 01:46:12 +0900

    a

--- src/components/ui/member/calendar/Dates.tsx
+++ src/components/ui/member/calendar/Dates.tsx
@@ -25,7 +25,7 @@ export default function Dates({ index, filter, dateInfo, password }: { index: nu
                                 flex flex-col items-center justify-center rounded
                                 ${date.toDateString() === new Date().toDateString() ? 'border-2 border-blue-500 font-bold' : ''}
                                 ${filter && info?.holiday.includes(filter) ? 'bg-yellow-200 hover:bg-yellow-300' : 
-                                    date.getMonth() === firstDate.getMonth() ? 'bg-gray-200 hover:bg-gray-300' : 'bg-gray-400 text-gray-800'
+                                    date.getMonth() === firstDate.getMonth() ? 'bg-gray-200 hover:bg-gray-300' : 'bg-gray-300 text-gray-600'
                                 }
                             `}
                         >
