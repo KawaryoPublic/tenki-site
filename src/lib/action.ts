1c0a0615b5eee7e836a93b015994fdc064a4ec81
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-09-24 11:51:15 +0900

    a

--- src/lib/action.ts
+++ src/lib/action.ts
@@ -1,8 +1,9 @@
 "use server";
 
 import { cookies } from "next/headers";
+import { TIER } from "./type";
 
 export const getTier = async () => {
     const cookieStore = await cookies();
-    return cookieStore.get("tier")?.value ?? "";
+    return cookieStore.get("tier")?.value as TIER ?? "";
 }
