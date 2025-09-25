bbface35b335f449583066911770f3577dbfb876
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-09-25 14:15:30 +0900

    a

--- src/components/ui/Button/RedButton.tsx
+++ src/components/ui/Button/RedButton.tsx
@@ -1,5 +1,5 @@
 export default function RedButton({ children, onClick }: { children: React.ReactNode, onClick?: () => void }) {
     return (
-        <button onClick={onClick} className="bg-red-500 text-white px-2 lg:px-3 py-1 text-sm lg:text-base rounded hover:bg-red-600">{children}</button>
+        <button onClick={onClick} className="bg-red-500 text-white px-2 md:px-3 py-1 text-sm md:text-base rounded hover:bg-red-600">{children}</button>
     );
 }
