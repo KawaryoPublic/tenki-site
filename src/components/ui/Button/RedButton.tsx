b7a82bbc613b1e6d2af9c834f866be4071dc0d38
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-09-27 14:17:12 +0900

    a

--- src/components/ui/Button/RedButton.tsx
+++ src/components/ui/Button/RedButton.tsx
@@ -3,7 +3,9 @@ import Link from "next/link";
 export default function RedButton({ children, onClick, href, className = "" }: { children: React.ReactNode, onClick?: () => void, href?: string, className?: string }) {
     return (
         href ? 
-        <Link href={href} className={`bg-red-500 text-white px-2 md:px-3 py-2 text-sm md:text-base rounded hover:bg-red-600 ${className}`}>{children}</Link> :
+        <Link href={href} className={`bg-red-500 text-white text-sm md:text-base rounded hover:bg-red-600 ${className}`}>
+            <soan className="px-2 md:px-3 py-1">{children}</soan>
+        </Link> :
         <button onClick={onClick} className={`bg-red-500 text-white px-2 md:px-3 py-1 text-sm md:text-base rounded hover:bg-red-600 ${className}`}>{children}</button>
     );
 }
