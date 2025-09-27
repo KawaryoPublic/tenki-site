1fa6c6d5ebd5adc66924b5c46b31e4e7596a2ee8
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-09-27 14:45:17 +0900

    a

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -8,7 +8,7 @@ export default async function Header() {
 
   return (
     <header className="text-gray-900 pt-4 pl-4 w-full flex items-center bg-gray-300 whitespace-nowrap">
-        <h1 className="text-xl md:text-3xl">天文気象部</h1>
+        <h1 className="text-xl md:text-3xl font-bold">天文気象部</h1>
         <nav className="flex text-base md:text-xl space-x-3 lg:space-x-4 p-2 md:p-4 overflow-x-auto">
           <Nav href="/" title="ホーム" tier={tier} allowAll />
           <Nav href="/about" title="アバウト" tier={tier} allowAll />
@@ -17,8 +17,10 @@ export default async function Header() {
           <Nav href="/storage/0" title="倉庫" tier={tier} allowStudent />
           <Nav href="/file" title="ファイル" tier={tier} />
         </nav>
-        <div className="flex-none ml-auto pr-2">
-          <Image src="/image/sign_in.jpg" alt="部員はこちら" width={980} height={980} className="w-5 h-5 md:w-7 md:h-7" />
+        <div className="flex-none ml-auto pr-4">
+          <Link href="/password">
+            <Image src="/image/sign_in.jpg" alt="部員はこちら" width={980} height={980} className="w-5 h-5 md:w-7 md:h-7" />
+          </Link>
         </div>
     </header>
   );
