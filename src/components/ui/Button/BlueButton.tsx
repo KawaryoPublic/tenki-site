23e30e55c235d20987e678045bb58a6c11d19b0b
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-09-27 13:46:18 +0900

    a

--- src/components/ui/Button/BlueButton.tsx
+++ src/components/ui/Button/BlueButton.tsx
@@ -3,7 +3,9 @@ import Link from "next/link";
 export default function BlueButton({ children, onClick, href, className = "" }: { children: React.ReactNode, onClick?: () => void, href?: string, className?: string }) {
     return (
         href ? 
-        <Link href={href} className={`bg-blue-500 text-white px-2 md:px-3 py-1 text-sm md:text-base rounded hover:bg-blue-600 ${className}`}>{children}</Link> :
+        <button className={`bg-blue-500 text-white text-sm md:text-base rounded hover:bg-blue-600 ${className}`}>
+            <Link href={href} className="px-2 md:px-3 py-1">{children}</Link>
+        </button> :
         <button onClick={onClick} className={`bg-blue-500 text-white px-2 md:px-3 py-1 text-sm md:text-base rounded hover:bg-blue-600 ${className}`}>{children}</button>
     );
 }
