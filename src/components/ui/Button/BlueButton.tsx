b11e705e36a97b3b6964f7ddcf41f43855687d12
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-09-27 13:40:28 +0900

    a

--- src/components/ui/Button/BlueButton.tsx
+++ src/components/ui/Button/BlueButton.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 
-export default function BlueButton({ children, onClick, href }: { children: React.ReactNode, onClick?: () => void, href?: string }) {
+export default function BlueButton({ children, onClick, href, className = "" }: { children: React.ReactNode, onClick?: () => void, href?: string, className?: string }) {
     return (
         href ? 
-        <Link href={href} className="bg-blue-500 text-white px-2 md:px-3 py-1 text-sm md:text-base rounded hover:bg-blue-600">{children}</Link> :
-        <button onClick={onClick} className="bg-blue-500 text-white px-2 md:px-3 py-1 text-sm md:text-base rounded hover:bg-blue-600">{children}</button>
+        <Link href={href} className={`bg-blue-500 text-white px-2 md:px-3 py-1 text-sm md:text-base rounded hover:bg-blue-600 ${className}`}>{children}</Link> :
+        <button onClick={onClick} className={`bg-blue-500 text-white px-2 md:px-3 py-1 text-sm md:text-base rounded hover:bg-blue-600 ${className}`}>{children}</button>
     );
 }
--- src/components/ui/Button/RedButton.tsx
+++ src/components/ui/Button/RedButton.tsx
@@ -1,5 +1,9 @@
-export default function RedButton({ children, onClick }: { children: React.ReactNode, onClick?: () => void }) {
+import Link from "next/link";
+
+export default function RedButton({ children, onClick, href, className = "" }: { children: React.ReactNode, onClick?: () => void, href?: string, className?: string }) {
     return (
-        <button onClick={onClick} className="bg-red-500 text-white px-2 md:px-3 py-1 text-sm md:text-base rounded hover:bg-red-600">{children}</button>
+        href ? 
+        <Link href={href} className={`bg-red-500 text-white px-2 md:px-3 py-1 text-sm md:text-base rounded hover:bg-red-600 ${className}`}>{children}</Link> :
+        <button onClick={onClick} className={`bg-red-500 text-white px-2 md:px-3 py-1 text-sm md:text-base rounded hover:bg-red-600 ${className}`}>{children}</button>
     );
 }
