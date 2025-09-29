4121c7ae3822da1de956190ca0b8eaa8f0e4ee16
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-09-29 14:39:56 +0900

    a

--- src/components/feature/notification/Notification.tsx
+++ src/components/feature/notification/Notification.tsx
@@ -5,12 +5,14 @@ import { NotificationType, TIER } from "@/lib/type";
 import BlueButton from "@/components/ui/Button/BlueButton";
 
 export default function Notification({ notification, tier }: { notification: NotificationType, tier: TIER }) {
+    const updatedAt = new Date(notification.updatedAt);
+
     return (
         <WhiteFrame>
             <div className="flex justify-between items-center mb-2 border-b pb-2">
                 <span className="text-xl lg:text-3xl font-bold">{notification.title}</span>
                 <div className="flex items-center gap-4">
-                    <span className="text-gray-800 text-xs lg:text-sm">{`${new Date(notification.updatedAt).getFullYear()}年${new Date(notification.updatedAt).getMonth() + 1}月${new Date(notification.updatedAt).getDate()}日`}</span>
+                    <span className="text-gray-800 text-xs lg:text-sm">{`${updatedAt.getFullYear() === (new Date()).getFullYear() ? "" : `${updatedAt.getFullYear()}年`}${updatedAt.getMonth() + 1}月${updatedAt.getDate()}日`}</span>
                     {
                         checkTier(tier) ? 
                         <>
