c88e878080e778a371dff76d6cc5e7bf2c8ee034
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-10-08 12:30:19 +0900

    a

--- src/app/(page)/manual/page.tsx
+++ src/app/(page)/manual/page.tsx
@@ -1,7 +1,33 @@
 export default function Home() {
     return (
-        <main>
-            <div>マニュアルです。</div>
-        </main>
+        <section>
+            <div className="flex flex-col gap-6">
+                <div className="flex flex-col gap-2">
+                    <h2 className="text-lg md:text-xl font-bold">いつもの観測</h2>
+                    <nav className="pl-1 flex flex-col gap-1">
+                        <Link href="/manual">気象観測</Link>
+                        <Link href="/manual">太陽観測</Link>
+                    </nav>
+                </div>
+                <div className="flex flex-col gap-2">
+                    <h2 className="text-lg md:text-xl font-bold">機材</h2>
+                    <nav className="pl-1 flex flex-col gap-1">
+                        
+                    </nav>
+                </div>
+                <div className="flex flex-col gap-2">
+                    <h2 className="text-lg md:text-xl font-bold">合宿</h2>
+                    <nav className="pl-1 flex flex-col gap-1">
+                        
+                    </nav>
+                </div>
+                <div className="flex flex-col gap-2">
+                    <h2 className="text-lg md:text-xl font-bold">文化祭</h2>
+                    <nav className="pl-1 flex flex-col gap-1">
+                
+                    </nav>
+                </div>
+            </div>
+        </section>
     )
 }
--- src/app/(page)/tool/page.tsx
+++ src/app/(page)/tool/page.tsx
@@ -7,10 +7,12 @@ export default async function Home() {
 
     return (
         checkTier(tier, false, true) &&
-        <nav className="flex flex-col">
-            <Link href="/tool/cloud?height=0">雲の分類</Link>
-            <Link href="/tool">黒点の分類(未定)</Link>
-            <a target="_blank" href="https://www.carina.gr.jp/~yamane/sun_pbl/index.php" rel="noopener noreferrer">太陽の自転軸</a>
-        </nav>
+        <section>
+            <nav className="flex flex-col">
+                <Link href="/tool/cloud?height=0">雲の分類</Link>
+                <Link href="/tool">黒点の分類(未定)</Link>
+                <a target="_blank" href="https://www.carina.gr.jp/~yamane/sun_pbl/index.php" rel="noopener noreferrer">太陽の自転軸</a>
+            </nav>
+        </section>
     )
 }
