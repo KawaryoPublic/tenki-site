2af6a9027bead1fd5a5557bf2825fcc0b9391079
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-10-08 10:55:16 +0900

    a

--- src/app/(page)/tool/cloud/page.tsx
+++ src/app/(page)/tool/cloud/page.tsx
@@ -8,7 +8,7 @@ export default async function Home({ searchParams }: { searchParams: { height: s
             <h1>{["CL", "CM", "CH"][Number(searchParams.height)]}の分類</h1>
             <div>
                 <p>Cb(積乱雲)はありますか?</p>
-                <button className="pr-4">ある</button> <button>ない</button>
+                <button className="pr-4">ある</button><button>ない</button>
             </div>
             ....
             <div>{["CL", "CM", "CH"][Number(searchParams.height)]}は4です。</div>
--- src/app/(page)/tool/page.tsx
+++ src/app/(page)/tool/page.tsx
@@ -7,10 +7,10 @@ export default async function Home() {
 
     return (
         checkTier(tier, false, true) &&
-        <nav>
+        <nav className="flex flex-col">
             <Link href="/tool/cloud?height=0">雲の分類</Link>
             <Link href="/tool/sunspot">黒点の分類(未定)</Link>
-            <Link href="https://www.carina.gr.jp/~yamane/sun_pbl/index.php">太陽の自転軸</Link>
+            <a target="_blank" href="https://www.carina.gr.jp/~yamane/sun_pbl/index.php" rel="noopener noreferrer">太陽の自転軸</a>
         </nav>
     )
 }
