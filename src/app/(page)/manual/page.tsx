eee6ae722eac30a4ed26f2b875e36fa4afd16ce4
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-10-10 11:22:24 +0900

    a

--- src/app/(page)/manual/page.tsx
+++ src/app/(page)/manual/page.tsx
@@ -1,34 +1,35 @@
+import WhiteFrame from "@/components/ui/WhiteFrame";
 import Link from "next/link";
 
 export default function Home() {
     return (
         <section>
-            <div className="flex flex-col gap-6">
-                <div className="flex flex-col gap-2">
-                    <h2 className="text-lg md:text-xl font-bold">いつもの観測</h2>
+            <div className="flex flex-col gap-4">
+                <WhiteFrame className="flex flex-col gap-2">
+                    <h2 className="text-xl md:text-2xl font-bold">いつもの観測</h2>
                     <nav className="pl-1 flex flex-col gap-1">
                         <Link href="/manual">気象観測</Link>
                         <Link href="/manual">太陽観測</Link>
                     </nav>
-                </div>
-                <div className="flex flex-col gap-2">
-                    <h2 className="text-lg md:text-xl font-bold">機材</h2>
+                </WhiteFrame>
+                <WhiteFrame className="flex flex-col gap-2">
+                    <h2 className="text-xl md:text-2xl font-bold">機材</h2>
                     <nav className="pl-1 flex flex-col gap-1">
                         
                     </nav>
-                </div>
-                <div className="flex flex-col gap-2">
-                    <h2 className="text-lg md:text-xl font-bold">合宿</h2>
+                </WhiteFrame>
+                <WhiteFrame className="flex flex-col gap-2">
+                    <h2 className="text-xl md:text-2xl font-bold">合宿</h2>
                     <nav className="pl-1 flex flex-col gap-1">
                         
                     </nav>
-                </div>
-                <div className="flex flex-col gap-2">
-                    <h2 className="text-lg md:text-xl font-bold">文化祭</h2>
+                </WhiteFrame>
+                <WhiteFrame className="flex flex-col gap-2">
+                    <h2 className="text-xl md:text-2xl font-bold">文化祭</h2>
                     <nav className="pl-1 flex flex-col gap-1">
                 
                     </nav>
-                </div>
+                </WhiteFrame>
             </div>
         </section>
     )
