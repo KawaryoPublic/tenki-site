ef49fce7f9f13a6f080ff66501dcf9bf20d38775
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-10-10 16:01:44 +0900

    a

--- src/app/(page)/manual/page.tsx
+++ src/app/(page)/manual/page.tsx
@@ -4,15 +4,17 @@ import Link from "next/link";
 
 export default function Home() {
     return (
-        <section className="w-full">
-            <BlueButton href="/manual/edit" className="mb-3">追加</BlueButton>
-            <div className="w-full flex flex-col gap-4">
-                <WhiteFrame className="flex flex-col gap-2">
-                    <div className="flex items-center">
+        <section className="flex flex-col gap-3 w-full">
+            <div>
+                <BlueButton href="/manual/edit">追加</BlueButton>
+            </div>
+            <div className="flex flex-col gap-4">
+                <WhiteFrame>
+                    <div className="flex justify-between items-center mb-2 border-b pb-2">
                         <h2 className="text-xl md:text-2xl font-bold">いつもの観測</h2>
                         <BlueButton href="/manual/edit" className="mr-auto">追加</BlueButton>
                     </div>
-                    <nav className="pl-1 flex flex-col gap-1">
+                    <nav className="underline flex flex-col gap-1">
                         <Link href="/manual">気象観測</Link>
                         <Link href="/manual">太陽観測</Link>
                     </nav>
