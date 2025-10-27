0c5ea2c2816af9b70714ccab8782f1a2a9538e81
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-10-27 13:12:35 +0900

    a

--- src/components/ui/notification/Form/EditNotificationForm.tsx
+++ src/components/ui/notification/Form/EditNotificationForm.tsx
@@ -5,8 +5,8 @@ import { redirect } from "next/navigation";
 import { Notification, TIER } from "@/lib/type";
 import { useState, useEffect } from "react";
 import BlueButton from "../../global/Button/BlueButton";
-import { DefaultInput } from "../../global/Input/DefaultInputWithDefaultValue";
-import { DefaultTextArea } from "../../global/Input/DefaultTextAreaWithDefaultValue";
+import DefaultInput from "../../global/Input/DefaultInputWithDefaultValue";
+import DefaultTextArea from "../../global/Input/DefaultTextAreaWithDefaultValue";
 
 export default function EditNotificationForm({ id }: { id: number }) {
     const [ notification, setNotification ] = useState<Notification>({id: -1, title: "", content: "", tier: "", updatedAt: new Date()});
