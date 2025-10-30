ecc8f3c34567dfea0065785daef5a85629088b6c
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-10-30 09:58:18 +0900

    a

---
+++ src/app/api/upload/route.tsx
@@ -0,0 +1,26 @@
+import { put } from '@vercel/blob';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(request: NextRequest) {
+    try {
+        const { files } = await request.json();
+
+        if(!files || !Array.isArray(files) || files.length === 0) {
+            return NextResponse.json({ error: "No files provided" }, { status: 400 });
+        }
+
+        const blobs = [];
+
+        for (const file of files) {
+            const blob = await put(file.name, file.data, {
+                access: 'public-read',
+            });
+            blobs.push(blob);
+        }
+    
+        return NextResponse.json({ blobs }, { status: 201 });
+    } catch (error) {
+        console.error("Error uploading a file:", error);
+        return NextResponse.json({ error: "Failed to upload a file" }, { status: 500 });
+    }
+}
