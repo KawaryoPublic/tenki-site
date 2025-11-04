875a9ddaacd9d9f125ef60513891cb2bdd8a62d7
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-11-04 14:20:09 +0900

    a

--- src/components/ui/global/Button/BlueButton.tsx
+++ src/components/ui/global/Button/BlueButton.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 import Link from "next/link";
 
-export default function BlueButton({ children, onClick, href, type = "submit", className = "" }: { children: ReactNode, onClick?: () => void, href?: string, type?: "button" | "submit" | "reset" | "undefined", className?: string }) {
+export default function BlueButton({ children, onClick, href, type, className = "" }: { children: ReactNode, onClick?: () => void, href?: string, type?: "button" | "submit" | "reset" | undefined, className?: string }) {
     return (
         href ? 
         <Link href={href} className={`bg-blue-500 text-white px-2 md:px-3 py-1 text-sm md:text-base rounded hover:bg-blue-600 ${className}`}>{children}</Link> :
