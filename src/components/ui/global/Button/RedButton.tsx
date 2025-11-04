4a1e29f92720e989595addbb7820ec1d04a0724a
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-11-04 14:18:01 +0900

    a

--- src/components/ui/global/Button/RedButton.tsx
+++ src/components/ui/global/Button/RedButton.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 import Link from "next/link";
 
-export default function RedButton({ children, onClick, href, type, className = "" }: { children: ReactNode, onClick?: () => void, href?: string, type?: string, className?: string }) {
+export default function RedButton({ children, onClick, href, type, className = "" }: { children: ReactNode, onClick?: () => void, href?: string, type?: "button" | "submit" | "reset" | "undefined", className?: string }) {
     return (
         href ? 
         <Link href={href} className={`bg-red-500 text-white px-2 md:px-3 py-[1.5px] text-sm md:text-base rounded hover:bg-red-600 ${className}`}>{children}</Link> :
