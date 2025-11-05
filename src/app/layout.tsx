65432323cac1e35cb233abdd8d88cc90f7a20869
KawaryoPublic <kawaryo.tenki.public@gmail.com>
2025-11-06 08:34:08 +0900

    Remove fixed positioning from background overlay

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
         <div className="fixed z-[-1] top-0 left-0 w-full h-screen">
           <Image src="/image/bg_pc.jpg" alt="背景" fill className="object-cover max-sm:hidden" />
           <Image src="/image/bg_phone.jpg" alt="背景" fill className="object-cover sm:hidden" />
-          <div className="w-full h-full fixed bg-black opacity-50" />
+          <div className="w-full h-full bg-black opacity-50" />
         </div>
         <Header />
         <main className="p-5 container flex-1 flex">
