a9bddcd164ae0ab1439d3b4da4edd1ded94cdbb6
KawaryoPublic <kawaryo.tenki.public@gmail.com>
2025-11-06 08:29:59 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,11 +15,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <body className="min-h-screen overflow-x-hidden flex flex-col items-center relative">
-        <div className="fixed z-[-2] top-0 left-0 w-full h-full">
+      <body className="min-h-screen overflow-x-hidden flex flex-col items-center">
+        <div className="fixed z-[-1] top-0 left-0 w-full h-screen">
           <Image src="/image/bg_pc.jpg" alt="背景" fill className="object-cover max-sm:hidden" />
-          <Image src="/image/bg_phone.jpg" alt="背景" fill className="fixed object-cover sm:hidden" />
-          <div className="w-full h-full fixed bg-black opacity-50 z-[-1]" />
+          <Image src="/image/bg_phone.jpg" alt="背景" fill className="object-cover sm:hidden" />
+          <div className="w-full h-full fixed bg-black opacity-50" />
         </div>
         <Header />
         <main className="p-5 container flex-1 flex">
