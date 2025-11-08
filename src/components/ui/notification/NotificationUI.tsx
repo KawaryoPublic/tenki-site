229f4a21056617caeba70c149b677f95e843c2ea
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-11-08 10:17:38 +0900

    a

--- src/components/ui/notification/NotificationUI.tsx
+++ src/components/ui/notification/NotificationUI.tsx
@@ -10,7 +10,7 @@ export default function NotificationUI({ notification, tier }: { notification: N
 
     return (
         <WhiteFrameUI>
-            <div className={`flex justify-between items-center ${file.tags.length !== 0 && "border-b pb-2 mb-2"}`}>
+            <div className={`flex justify-between items-center ${notification.tags.length !== 0 && "border-b pb-2 mb-2"}`}>
                 <Link className="text-xl lg:text-3xl font-bold" href={`notification/${notification.id}`}>{notification.title}</Link>
                 <div className="flex items-center gap-4">
                     <span className="text-gray-800 text-xs lg:text-sm">{`${updatedAt.getFullYear() === (new Date()).getFullYear() ? "" : `${updatedAt.getFullYear()}年`}${updatedAt.getMonth() + 1}月${updatedAt.getDate()}日`}</span>
