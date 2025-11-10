16327c716a8882060c470c49905403d100915e80
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-11-10 13:11:02 +0900

    a

--- src/components/ui/notification/NotificationDetailUI.tsx
+++ src/components/ui/notification/NotificationDetailUI.tsx
@@ -3,7 +3,7 @@ import BlueButton from "../global/Button/BlueButton";
 import WhiteFrameUI from "../global/WhiteFrameUI";
 import { Notification, TIER } from "@/lib/type";
 import DeleteNotificationButton from "./Button/DeleteNotificationButton";
-import Image from "next/image";
+import Link from "next/link";
 
 export default function NotificationDetailUI({ notification, tier }: { notification: Notification, tier: TIER }) {
     return (
@@ -26,7 +26,7 @@ export default function NotificationDetailUI({ notification, tier }: { notificat
             <div>
                 {
                     notification.urls.map((url, index) => (
-                        <Image key={index} src={url} alt={`${url}`} className="my-2 max-w-full h-auto" />
+                        <Link key={index} href={url} target="_blank" className="text-blue-500 underline block">{url}</Link>
                     ))
                 }
             </div>
