7873a0145c6c9589b14f1ec269435aa93e70a280
KawaryoPublic <kawaryo.tenki.public@gmail.com>
2025-11-11 15:19:53 +0900

    Update DefaultFile.tsx

--- src/components/ui/global/Form/DefaultFile.tsx
+++ src/components/ui/global/Form/DefaultFile.tsx
@@ -5,7 +5,7 @@ import FileLinkUI from "../FileLinkUI";
 import { Dispatch, SetStateAction, useState, useRef, RefObject } from "react";
 
 export default function DefaultFile({ title, name, defaultFiles = [], setDefaultFiles }: { title: string, name: string, defaultFiles?: { url: string, filename: string }[], setDefaultFiles?: Dispatch<SetStateAction<{ url: string, filename: string }[]>> }) {
-    const [ refs, setRefs ] = useState<RefObject<HTMLInputElement>[]>([]);
+    const [ refs, setRefs ] = useState<RefObject<HTMLInputElement | null>[]>([]);
 
     return (
         <div className="flex flex-col gap-2">
