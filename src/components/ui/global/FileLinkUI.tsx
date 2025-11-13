a8d4fc57c6260a8e56c4cb379562c64996627c01
Kawaryo <kawaryo.tenki.public@gmail.com>
2025-11-13 13:20:48 +0900

    a

--- src/components/ui/global/FileLinkUI.tsx
+++ src/components/ui/global/FileLinkUI.tsx
@@ -2,6 +2,6 @@ import Link from "next/link";
 
 export default function FileLinkUI({ url, filename, className = "" }: { url: string, filename: string, className?: string }) {
     return (
-        <Link href={url} target="_blank" className={`border rounded-md px-2 md:py-4 overflow-hidden whitespace-nowrap ${className}`}>{filename}</Link>
+        <Link href={url} target="_blank" className={`border rounded-md px-2 overflow-hidden whitespace-nowrap ${className}`}>{filename}</Link>
     )
 }
