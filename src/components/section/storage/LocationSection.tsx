32caecfa47b789024d3e791e7c79e4a7c3c0253e
Kawaryo <kawaryo.tenki.public@gmail.com>
2026-01-14 11:09:45 +0900

    a

--- src/components/section/storage/LocationSection.tsx
+++ src/components/section/storage/LocationSection.tsx
@@ -7,17 +7,15 @@ import Link from "next/link";
 export default function LocationSection({ location, tier }: { location: string, tier: TIER }) {
     return (
         <section className="flex-1 flex flex-col gap-4 items-center">
-            <div>
-                <nav className="flex flex gap-2">
-                    {
-                        Object.entries(LOCATIONS_LABELS).map(([value, label]) => (
-                            <WhiteFrameUI>
-                                <Link href={`/storage/${value}`}>{label}</Link>
-                            </WhiteFrameUI>
-                        ))
-                    }
-                </nav>
-            </div>
+             <WhiteFrameUI className="flex flex gap-2">
+                {
+                    Object.entries(LOCATIONS_LABELS).map(([value, label]) => (
+                        <div>
+                            <Link href={`/storage/${value}`}>{label}</Link>
+                        </div>
+                    ))
+                }
+                </WhiteFrameUI>
             <div className="w-full flex-1 flex flex gap-2">
                 <div>
                     Location Map
