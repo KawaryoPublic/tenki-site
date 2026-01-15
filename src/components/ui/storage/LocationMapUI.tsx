02117c071c1f8e8024e2d95574afb1fb962eb84b
Kawaryo <kawaryo.tenki.public@gmail.com>
2026-01-15 13:59:24 +0900

    a

--- src/components/ui/storage/LocationMapUI.tsx
+++ src/components/ui/storage/LocationMapUI.tsx
@@ -3,7 +3,7 @@ import WhiteFrameUI from "../global/WhiteFrameUI";
 export default function LocationMapUI({ location }: { location: string }) {
     return (
         <WhiteFrameUI className="flex-1 flex items-center justify-center">
-            <div className="aspect-square object-contain border-2"></div>
+            <div className="flex-1 aspect-square object-contain border-2"></div>
         </WhiteFrameUI>
     )
 }
