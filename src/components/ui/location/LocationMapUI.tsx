fa860264b44a9f6ed58fcc02539a3670696fecbc
Kawaryo <kawaryo.tenki.public@gmail.com>
2026-01-20 11:07:54 +0900

    a

--- src/components/ui/location/LocationMapUI.tsx
+++ src/components/ui/location/LocationMapUI.tsx
@@ -7,7 +7,7 @@ export default function LocationMapUI({ location }: { location: Location }) {
             <div className="flex justify-between items-center border-b pb-2">
                 <h2 className="text-xl md:text-2xl font-bold">{location.name}の地図</h2>
             </div>
-            <div className={`flex-1 object-contain border-2`} style={{ aspectRatio: size[0] / size[1]}}></div>
+            <div className={`flex-1 object-contain border-2`} style={{ aspectRatio: location.size[0] / location.size[1]}}></div>
         </WhiteFrameUI>
     )
 }
