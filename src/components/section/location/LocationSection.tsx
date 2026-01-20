9a438158839914822c3159d4a26527fa8b4d5c56
Kawaryo <kawaryo.tenki.public@gmail.com>
2026-01-20 12:54:40 +0900

    a

--- src/components/section/location/LocationSection.tsx
+++ src/components/section/location/LocationSection.tsx
@@ -31,18 +31,18 @@ export default function LocationSection({ id, tier }: { id: number, tier: TIER }
     return (
         loading ? <div className="flex-1 flex flex-col items-center font-bold text-xl">Loading...</div> :
         !location ? <div className="flex-1 flex flex-col items-center font-bold text-xl">倉庫を読み込めませんでした</div> :
-        <section className="flex-1 flex flex gap-4 items-center">
-            <LocationMapUI location={location} />
-            <div className="h-full flex-1 flex flex-col gap-2">
-                <WhiteFrameUI className="flex justify-between items-center">
-                    {
-                        locations.map((location, i) => (
-                            <div key={i}>
-                                <Link href={`/storage/${location.id}`} className={`text-lg md:text-xl p-1 font-bold ${location.id === id ? "underline" : ""} hover:bg-gray-400`}>{location.name}</Link>
-                            </div>
-                        ))
-                    }
-                </WhiteFrameUI>
+        <section className="flex-1 flex flex-col gap-4 items-center">
+            <WhiteFrameUI className="flex justify-center gap-4 md:gap-8">
+                {
+                    locations.map((location, i) => (
+                        <div key={i}>
+                            <Link href={`/storage/${location.id}`} className={`text-lg md:text-xl p-1 font-bold ${location.id === id ? "underline" : ""} hover:bg-gray-400`}>{location.name}</Link>
+                        </div>
+                    ))
+                }
+            </WhiteFrameUI>
+            <div className="w-full flex-1 flex flex-col md:flex-row gap-4 lg:gap-8">
+                <LocationMapUI location={location} />
                 <LocationDetailUI location={location} tier={tier} />
             </div>
         </section>
