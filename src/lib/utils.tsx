f5c1a99beeae3b51401b6c84e51a3b7a29e3efe2
Kawaryo <kawaryo.tenki.public@gmail.com>
2026-02-03 12:53:13 +0900

    a

--- src/lib/utils.tsx
+++ src/lib/utils.tsx
@@ -21,10 +21,12 @@ export const formatDate = (date: string) => {
 }
 
 export const fitToParentSize = (parentRef: RefObject<HTMLElement | null>, normalPadding: number, largePadding: number, aspectRatio: number) => {
+    if(parentRef.current) return [0, 0];
+
     const padding = window.innerWidth > 768 ? largePadding : normalPadding;
 
-    const parentWidth = parentRef?.current?.offsetWidth - padding;
-    const parentHeight = parentRef?.current?.offsetHeight - padding;
+    const parentWidth = parentRef.current.offsetWidth - padding;
+    const parentHeight = parentRef.current.offsetHeight - padding;
 
     return parentWidth / parentHeight < aspectRatio ? [parentWidth, parentWidth / aspectRatio] : [parentHeight * aspectRatio, parentHeight];
 }
