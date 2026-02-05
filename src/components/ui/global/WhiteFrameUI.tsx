745164336ec52405f6cdbc9e8bdd2cbedc53cf48
Kawaryo <kawaryo.tenki.public@gmail.com>
2026-02-05 16:39:16 +0900

    a

--- src/components/ui/global/WhiteFrameUI.tsx
+++ src/components/ui/global/WhiteFrameUI.tsx
@@ -1,6 +1,6 @@
-import { ReactNode } from "react";
+import { ReactNode, CSSProperties } from "react";
 
-export default function WhiteFrameUI({ children, style, className = "" }: { children: ReactNode, style: any, className?: string }) {
+export default function WhiteFrameUI({ children, style, className = "" }: { children: ReactNode, style?: CSSProperties, className?: string }) {
   return (
     <div style={style} className={`text-gray-900 bg-gray-200 rounded-lg shadow-md p-2 md:p-4 ${className}`}>
       {children}
