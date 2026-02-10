f482ca78fe1d91907cd6106d21265ca71b665fdb
Kawaryo <kawaryo.tenki.public@gmail.com>
2026-02-11 02:34:31 +0900

    a

--- src/components/ui/role/RoleDetailUI.tsx
+++ src/components/ui/role/RoleDetailUI.tsx
@@ -13,7 +13,7 @@ export default function RoleDetailUI({ id }: { id: number }) {
         <section className="w-full">
             <WhiteFrameUI className="flex flex-col gap-8">
                 <div className="flex items-center gap-4">
-                    <div href={`role/${role.id}`} className="w-12 relative flex aspect-square">
+                    <div className="w-12 relative flex aspect-square">
                         <Image src="/image/role_test.png" alt="Role Icon" fill sizes="w-full h-full" />
                     </div>
                     <div className="flex-1 whitespace-pre-wrap">
