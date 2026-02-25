f2098d61de6f39512102eee81c0757073685c962
Kawaryo <kawaryo.tenki.public@gmail.com>
2026-02-25 10:55:55 +0900

    a

--- src/app/api/cron/auto_deletion/route.ts
+++ src/app/api/cron/auto_deletion/route.ts
@@ -1,9 +1,30 @@
 import prisma from "@/lib/prisma";
+import { Notification } from "@/lib/types";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
     try {
-        return NextResponse.json({ message: "auto_deletion" }, { status: 200 });
+        const notifications = await fetch("/api/notification").then(res => res.json()) as Notification[];
+        const date_info = await fetch("/api/calendar/date_info").then(res => res.json()) as Notification[];
+        const today = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate());
+        const temp1 = [];
+        const temp2 = [];
+
+        for(const notification of notifications) {
+            const updatedAt = new Date(notification.updatedAt);
+            if(new Date(updatedAt.getFullYear(), updatedAt.getMonth(), updatedAt.getDate() + 7) > today) {
+                temp1.push(notification);
+            }
+        }
+
+        for(const info of date_info) {
+            const updatedAt = new Date(info.updatedAt);
+            if(new Date(updatedAt.getFullYear(), updatedAt.getMonth(), updatedAt.getDate() + 7) > today) {
+                temp2.push(info);
+            }
+        }
+
+        return NextResponse.json({ message: temp1.join(", ") + " | " + temp2.join(",") }, { status: 200 });
     } catch(error) {
         console.error("Error running auto deletion:", error);
         return NextResponse.json({ error: "Failed to run auto deletion" }, { status: 500 });
--- src/app/api/cron/observation_notification/route.ts
+++ src/app/api/cron/observation_notification/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
     try {
-        return NextResponse.json({ message: "auto_deletion" }, { status: 200 });
+        return NextResponse.json({ message: "observation_notification" }, { status: 200 });
     } catch(error) {
         console.error("Error notifying an observation:", error);
         return NextResponse.json({ error: "Failed to notify an observation" }, { status: 500 });
